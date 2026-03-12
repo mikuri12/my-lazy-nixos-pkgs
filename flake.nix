36051eb36dd8a0c9cb848250fd642ea63c4cb0f4
mikuri <navaljotap@gmail.com>
2026-03-12 11:58:53 -0600

    Add initial flake.nix configuration

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "My Lazy pkgs waaa";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    eachSystem = fn:
+      nixpkgs.lib.genAttrs
+      (import systems)
+      (system: fn (import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg: builtins.elem (pkg.pname or "") [
+          "spotiflac"
+        ];
+      }));
+
+    pkgsDir = builtins.readDir ./pkgs;
+    dirs = builtins.filter (
+      name:
+        pkgsDir.${name} == "directory"
+        && builtins.hasAttr "package.nix" (builtins.readDir (./pkgs/${name}))
+    ) (builtins.attrNames pkgsDir);
+  in {
+    packages =
+      eachSystem (pkgs:
+        pkgs.lib.genAttrs dirs (name:
+          pkgs.callPackage (./pkgs/${name}/package.nix) {}));
+
+    formatter = eachSystem (pkgs: pkgs.alejandra);
+
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [pkgs.alejandra pkgs.git];
+      };
+    });
+  };
+}
