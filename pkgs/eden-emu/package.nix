93888cc8146d04e21be82b033c114c85adcc4018
mikuri <navaljotap@gmail.com>
2026-05-14 12:30:28 -0600

    Update package.nix

--- pkgs/eden-emu/package.nix
+++ pkgs/eden-emu/package.nix
@@ -1,23 +1,43 @@
 {
   lib,
-  appimageTools,
+  stdenv,
   fetchurl,
+  makeWrapper,
+  appimage-run,
 }:
-appimageTools.wrapType1 {
+stdenv.mkDerivation rec {
   pname = "eden";
   version = "0.1.1";
 
   src = fetchurl {
-    url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v0.1.1/Eden-Linux-v0.1.1-amd64-gcc-standard.AppImage";
+    url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v${version}/Eden-Linux-v${version}-amd64-gcc-standard.AppImage";
     hash = "sha256-B3iU8SmwI44hUmbfFuSKQWriemulIXoRJF0atBfioeg=";
   };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  dontUnpack = true;
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp $src $out/bin/eden.AppImage
+    chmod +x $out/bin/eden.AppImage
+
+    makeWrapper ${lib.getExe appimage-run} $out/bin/eden \
+      --add-flags "$out/bin/eden.AppImage"
+
+    runHook postInstall
+  '';
 
   meta = {
     description = "Nintendo Switch emulator forked from yuzu";
     homepage = "https://eden-emu.dev";
     license = lib.licenses.gpl3Plus;
     mainProgram = "eden";
-    platforms = ["x86_64-linux"];
-    sourceProvenance = [lib.sourceTypes.binaryNativeCode];
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
   };
 }
