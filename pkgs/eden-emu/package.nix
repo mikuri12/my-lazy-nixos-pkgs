b9ed1e662a9c2a5f71d64856d08fd04f0f9406bf
mikuri <navaljotap@gmail.com>
2026-05-14 12:38:46 -0600

    Update Eden emulator package source URL and install method

--- pkgs/eden-emu/package.nix
+++ pkgs/eden-emu/package.nix
@@ -2,33 +2,24 @@
   lib,
   stdenv,
   fetchurl,
-  makeWrapper,
-  appimage-run,
 }:
 stdenv.mkDerivation rec {
   pname = "eden";
   version = "0.1.1";
 
   src = fetchurl {
-    url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v${version}/Eden-Linux-v${version}-amd64-gcc-standard.AppImage";
+    url = "https://stable.eden-emu.dev/v${version}/Eden-Linux-v${version}-amd64-gcc-standard.AppImage";
     hash = "sha256-B3iU8SmwI44hUmbfFuSKQWriemulIXoRJF0atBfioeg=";
   };
-
-  nativeBuildInputs = [ makeWrapper ];
 
   dontUnpack = true;
   dontBuild = true;
 
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out/bin
-    cp $src $out/bin/eden.AppImage
-    chmod +x $out/bin/eden.AppImage
-
-    makeWrapper ${lib.getExe appimage-run} $out/bin/eden \
-      --add-flags "$out/bin/eden.AppImage"
-
+    cp $src $out/bin/eden
+    chmod +x $out/bin/eden
     runHook postInstall
   '';
 
