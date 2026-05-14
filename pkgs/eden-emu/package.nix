43641c23ffdb9fa4abf1628cc1e6a7a20b8add3e
mikuri <navaljotap@gmail.com>
2026-05-14 12:28:45 -0600

    Refactor Eden emulator package definition

--- pkgs/eden-emu/package.nix
+++ pkgs/eden-emu/package.nix
@@ -3,37 +3,14 @@
   appimageTools,
   fetchurl,
 }:
-let
-  version = "0.1.1";
+appimageTools.wrapType1 {
   pname = "eden";
+  version = "0.1.1";
 
   src = fetchurl {
-    url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v${version}/Eden-Linux-v${version}-amd64-gcc-standard.AppImage";
+    url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v0.1.1/Eden-Linux-v0.1.1-amd64-gcc-standard.AppImage";
     hash = "sha256-B3iU8SmwI44hUmbfFuSKQWriemulIXoRJF0atBfioeg=";
   };
-
-  appimageContents = appimageTools.extractType2 {
-    inherit pname version src;
-  };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
-
-  extraInstallCommands = ''
-    install -Dm444 ${appimageContents}/eden.desktop \
-      $out/share/applications/eden.desktop
-
-    substituteInPlace $out/share/applications/eden.desktop \
-      --replace-fail "Exec=eden" "Exec=${pname}"
-
-    if [ -f ${appimageContents}/eden.svg ]; then
-      install -Dm444 ${appimageContents}/eden.svg \
-        $out/share/icons/hicolor/scalable/apps/eden.svg
-    elif [ -f ${appimageContents}/eden.png ]; then
-      install -Dm444 ${appimageContents}/eden.png \
-        $out/share/icons/hicolor/256x256/apps/eden.png
-    fi
-  '';
 
   meta = {
     description = "Nintendo Switch emulator forked from yuzu";
