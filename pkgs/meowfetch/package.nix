98623b2d2409eb768155b32dfcbb33ea210227ea
mikuri12 <navaljotap@gmail.com>
2026-06-11 15:25:51 -0600

    meowfetch
    add meowfetch

---
+++ pkgs/meowfetch/package.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+}:
+
+buildGoModule rec {
+  pname = "meowfetch";
+  version = "1.0.1-unstable";
+
+  src = fetchFromGitHub {
+    owner = "faynopi";
+    repo = "meowfetch";
+    rev = "e5424bec2395f37a654b5f0531f0baaaac6b642b";
+    hash = "sha256-2T1kimxrg4SfuWDx17N5SKmdvybHOXy4a4bXf80+09U=";
+  };
+
+  vendorHash = "sha256-PnkXXNr+kIige1YB/vEG+sYI0X/rr+6Gtcnb0rW4YK0=";
+
+  subPackages = ["."];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  nativeBuildInputs = [installShellFiles];
+
+  postInstall = ''
+    # Install man page
+    mkdir -p $out/share/man/man1
+    sed "s/{VERSION}/${version}/g" < meowfetch.1 > $out/share/man/man1/meowfetch.1
+
+    # Install default config
+    mkdir -p $out/share/meowfetch
+    cp configs/meow.conf $out/share/meowfetch/meow.conf
+  '';
+
+  meta = with lib; {
+    description = "Minimal, fast and customizable system information fetcher written in Go";
+    homepage = "https://github.com/faynopi/meowfetch";
+    license = licenses.mit;
+    maintainers = [];
+    platforms = platforms.linux;
+    mainProgram = "meowfetch";
+  };
+}
