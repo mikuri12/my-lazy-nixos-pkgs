930ae3ef759d4c52e89e73cacb842db640213ffe
mikuri12 <170357968+mikuri12@users.noreply.github.com>
2026-08-03 10:06:27 +0000

    chore: bump de versiones/hashes (nix-update)

--- pkgs/helium/package.nix
+++ pkgs/helium/package.nix
@@ -4,10 +4,10 @@
   fetchurl,
 }: let
   pname = "helium";
-  version = "0.14.7.1";
+  version = "0.15.1.1";
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-JPsCvue71hlyS9woHsauX5xM/2PUJ+n8VEjOFquUDno=";
+    hash = "sha256-qz3w+nnvBgkpHT3E34dv4DvFuYlyzTAyg9tPYJFWs3o=";
   };
 
   appimageContents = appimageTools.extractType2 {inherit pname version src;};
